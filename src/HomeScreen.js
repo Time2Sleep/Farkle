6d8c8624e5041ec1693d3d980a6d6d321c36d10f
Kroca <khoazat1@gmail.com>
2020-08-23 21:34:53 +0300

    [Navigation]

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,39 +1,53 @@
-import React, { Component } from 'react'
-import {View, ScrollView} from 'react-native'
-import Header from './Header'
-import ImageCard from './ImageCard'
-import data from './data.json'
+import * as React from 'react';
+import {Button, View, Text, ScrollView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Header from "./Header";
+import ImageCard from "./ImageCard";
 import styles from './CSS';
+import data from './data.json'
 
+function HomeScreen({navigation}) {
+        return (
+            <View>
+                    <Header title={"Hello world"}/>
+                    <ScrollView>
+                            <View style={styles.main}>
+                                    {data.map(item => (
+                                        <ImageCard
+                                            data={item}
+                                            key={item.id}
+                                            onPress={() => navigation.navigate('Details')}
+                                        />
+                                    ))
 
-class HomeScreen extends Component{
-        state = {
-                title: "GAMES"
-        }
+                                    }
+                            </View>
+                    </ScrollView>
 
-        render() {
-                 return (
-                    <View>
-                            <Header title={this.state.title}/>
-                            <ScrollView>
-                                    <View style={styles.main}>
-                                            {data.map(item =>(
-                                                <ImageCard
-                                                    data={item}
-                                                    key={item.id}
-                                                    onPress={()=>this.props.navigation.navigate('Main')}
-                                                />
-                                            ))
-
-                                            }
-                                    </View>
-                            </ScrollView>
-
-                    </View>
-                )
-        }
-
-
+            </View>
+        );
 }
 
-export default HomeScreen;
+function DetailsScreen() {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text>Details Screen</Text>
+            </View>
+        );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+        return (
+            <NavigationContainer>
+                    <Stack.Navigator initialRouteName="Home">
+                            <Stack.Screen name="Home" component={HomeScreen}/>
+                            <Stack.Screen name="Details" component={DetailsScreen}/>
+                    </Stack.Navigator>
+            </NavigationContainer>
+        );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
-import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./HomeScreen";
+import DetailsScreen from "./DetailsScreen";
 
+const Stack = createStackNavigator();
 
-export default StackNavigator(
-    {
-            Main: {
-                    screen: HomeScreen
-            },
-            Details:{
-                    screen: DetailsScreen
-            }
-    },
-    {
-            initialRouteName: 'Main'
-    }
-)
+ export function MyStack() {
+        return (
+            <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Notifications" component={DetailsScreen} />
+            </Stack.Navigator>
+        );
+}
