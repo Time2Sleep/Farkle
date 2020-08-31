6cd81b40aeff5177e14d1300d4d32c819e75da23
Kroca <khoazat1@gmail.com>
2020-08-31 20:46:59 +0300

    clickable

--- src/Farkle/GameScreen.js
+++ src/Farkle/GameScreen.js
@@ -1,50 +1,51 @@
 import React, {useState, Component} from 'react'
 import {View, Text, TouchableOpacity, StyleSheet, ImageBackground} from 'react-native'
 
-let text = "TOUCH ME"
+
 
 function GameScreen({navigation}) {
+        var [text, setText] = useState("TOUCH ME");
         const {container, image, selfScoreContainer, selfScoreText, opponentScoreContainer, opponentScoreText, button, pauseButton, touchable} = styles
-        let scoreText = "ВСЕГО /2000: \n0 \nРАУНД: \n0 \nБРОСОК: \n0 "
-        return(
-                <View style={container}>
-                        <ImageBackground source={require('../../assets/BG.jpg')} style={image}>
+        let scoreText = "ВСЕГО /2000: \n0 \nРАУНД: \n0 \nБРОСОК: \n0 ";
+        return (
+            <View style={container}>
+                    <ImageBackground source={require('../../assets/BG.jpg')} style={image}>
 
-                                <View  style={{flexDirection: 'row'}}>
-                                        <View>
-                                                <TouchableOpacity style={button} onPress={() => navigation.navigate('Menu')}>
-                                                                <Text style={pauseButton}> | | </Text>
-                                                </TouchableOpacity>
-                                        </View>
-                                        <View style={opponentScoreContainer}>
-                                                <Text style={opponentScoreText}>
-                                                        {scoreText}
-                                                </Text>
-                                        </View>
-                                </View>
-                                <View style={selfScoreContainer}>
-                                        <TouchableOpacity style={touchable} onPress={() => throwDices()}>
-                                                <MainContent text={text}/>
-                                        <Text style={selfScoreText}>
-                                                {scoreText}
-                                        </Text>
-                                        </TouchableOpacity>
-                                </View>
-                        </ImageBackground>
-                </View>
-        )
-
+                            <View style={{flexDirection: 'row'}}>
+                                    <View>
+                                            <TouchableOpacity style={button}
+                                                              onPress={() => navigation.navigate('Menu')}>
+                                                    <Text style={pauseButton}> | | </Text>
+                                            </TouchableOpacity>
+                                    </View>
+                                    <View style={opponentScoreContainer}>
+                                            <Text style={opponentScoreText}>
+                                                    {scoreText}
+                                            </Text>
+                                    </View>
+                            </View>
+                            <View style={selfScoreContainer}>
+                                    <TouchableOpacity style={touchable} onPress={() => throwDices()}>
+                                            <MainContent text={text}/>
+                                            <Text style={selfScoreText}>
+                                                    {scoreText}
+                                            </Text>
+                                    </TouchableOpacity>
+                            </View>
+                    </ImageBackground>
+            </View>
+        );
+        function throwDices() {
+                setText("lelel");
+        }
 }
 
-function throwDices(){
-        text = "lelel"
-}
 
-function MainContent({text}){
-        return(
-        <View>
-                <Text style={styles.contentText}> {text}</Text>
-        </View>
+function MainContent({text}) {
+        return (
+            <View>
+                    <Text style={styles.contentText}> {text}</Text>
+            </View>
         )
 }
 
@@ -53,48 +54,48 @@ const styles = StyleSheet.create({
         container: {
                 flex: 1,
         },
-        image:{
+        image: {
                 flex: 1,
                 width: '100%',
                 justifyContent: "center"
         },
-        selfScoreContainer:{
+        selfScoreContainer: {
                 flex: 1,
                 justifyContent: 'flex-end',
         },
-        selfScoreText:{
+        selfScoreText: {
                 color: '#fff',
                 fontWeight: 'bold',
                 fontSize: 18,
                 paddingLeft: 10
         },
-        opponentScoreContainer:{
+        opponentScoreContainer: {
                 flex: 1,
                 justifyContent: 'flex-start',
                 alignItems: 'flex-end',
                 paddingTop: 20
         },
-        opponentScoreText:{
+        opponentScoreText: {
                 color: '#fff',
                 fontWeight: 'bold',
                 fontSize: 18,
                 paddingRight: 10,
                 textAlign: 'right'
         },
-        button:{
+        button: {
                 paddingTop: 25,
                 paddingLeft: 10,
         },
-        pauseButton:{
+        pauseButton: {
                 color: '#fff',
                 fontWeight: 'bold',
                 fontSize: 32,
         },
-        touchable:{
+        touchable: {
                 flex: 1,
                 justifyContent: 'flex-end',
         },
-        contentText:{
+        contentText: {
                 alignSelf: 'center',
                 fontSize: 45,
                 color: 'white',
